be2f5aa38767740fc3a83f3e04600dee8a3edc84
Pglagarto2005 <santiblo05@gmail.com>
2026-04-16 10:18:30 -0500

    Update Canvas.py

--- Canvas.py
+++ Canvas.py
@@ -24,7 +24,7 @@ with st.sidebar:
 
 canvas_result = st_canvas(
     fill_color ="rgba(255, 165, 0, 0.3)",
-    stoke_width=stroke_width,
+    stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
     width=canvas_width,
