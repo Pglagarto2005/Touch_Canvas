09f639e060f0d75f2aee3a85a1146e0c41dca077
Pglagarto2005 <santiblo05@gmail.com>
2026-04-16 10:28:25 -0500

    Update Canvas.py

--- Canvas.py
+++ Canvas.py
@@ -4,31 +4,30 @@ from streamlit_drawable_canvas import st_canvas
 st.title("PAINT")
 
 with st.sidebar:
-  st.subheader("propiedades del tablero")
-
-
-  st.subheader("Tamaño del tablero")
-  canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
-  canvas_height = st.slider("Alto del tablero", 200, 600, 300, 50)
-
-  drawing_mode = st.selectbox(
-    "Trazos",
-    ("Pincel", "Linea", "Rectangulo", "Circulo", "Transform", "Poligono", "Point"),
-  )
-
-  stroke_width = st.slider ("Selecciona el tamaño de tu Trazo", 1, 30, 15)
-
-  stroke_color = st.color_picker("Color de trazo", "#FFFFFF")
-
-  bg_color = st.color_picker("Color del fondo", "#000000")
+    st.subheader("Propiedades del tablero")
+  
+    st.subheader("Tamaño del tablero")
+    canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
+    canvas_height = st.slider("Alto del tablero", 200, 600, 300, 50)
+  
+    drawing_mode = st.selectbox(
+      "Trazos",
+      ("Pincel", "Linea", "Rectangulo", "Circulo", "Transform", "Poligono", "Point"),
+    )
+  
+    stroke_width = st.slider ("Selecciona el tamaño de tu Trazo", 1, 30, 15)
+  
+    stroke_color = st.color_picker("Color de trazo", "#FFFFFF")
+  
+    bg_color = st.color_picker("Color del fondo", "#000000")
 
 canvas_result = st_canvas(
     fill_color ="rgba(255, 165, 0, 0.3)",
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
-    width=canvas_width,
     height=canvas_height,
+    width=canvas_width,
     drawing_mode=drawing_mode,
     key=f"canvas_{canvas_width}_{canvas_height}",
 )
